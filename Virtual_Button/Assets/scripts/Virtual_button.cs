493fbc3b4fa28fcf8a18987c497ef83f4d3e4d4f
Mohit Kumar <61533525+mkumarj786@users.noreply.github.com>
2020-04-09 12:45:00 +0530

    initial

---
+++ Virtual_Button/Assets/scripts/Virtual_button.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Virtual_button : MonoBehaviour , IVirtualButtonEventHandler
+{
+    public GameObject sphere,cube;
+    VirtualButtonBehaviour vrb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vrb = GetComponentInChildren<VirtualButtonBehaviour>();
+        vrb.RegisterEventHandler(this);
+        cube.SetActive(true);
+        sphere.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb){
+        cube.SetActive(false);
+        sphere.SetActive(true);
+
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb){
+
+        cube.SetActive(true);
+        sphere.SetActive(false);
+    }
+ 
+    
+
+}
